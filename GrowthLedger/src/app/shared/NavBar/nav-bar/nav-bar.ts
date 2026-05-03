8724987dd73fd597c4b21631562845280b8f9003
Tamanpreet Singh <tamanpreetsingh76@gmail.com>
2026-05-03 11:34:10 +0530

    Nav abr update

--- GrowthLedger/src/app/shared/NavBar/nav-bar/nav-bar.ts
+++ GrowthLedger/src/app/shared/NavBar/nav-bar/nav-bar.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatDividerModule } from '@angular/material/divider';
+// nav-bar.ts
 
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [MatIconModule,MatMenuModule,MatDividerModule],
+  imports: [MatIconModule],
   templateUrl: './nav-bar.html',
-  styleUrl: './nav-bar.css',
+  styleUrl: './nav-bar.css'
 })
 export class NavBar {
+
+  menuOpen = false;
+
+  // later connect with service
+  kycDone = false;
+
+  constructor(private router: Router) {}
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
+  handleCTA() {
+    if (this.kycDone) {
+      this.router.navigate(['/business/investors']);
+    } else {
+      this.router.navigate(['/funder-kyc']);
+    }
+
+    this.closeMenu();
+  }
 
 }
